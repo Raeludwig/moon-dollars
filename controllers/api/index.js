402548b8457c55278070e5c2908e562136d3dbfc
Raeludwig <racheleludwig@gmail.com>
2023-03-30 18:22:16 -0600

    put the routes in the correct locations

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,4 +5,10 @@ const exampleRouter = require('./example-router');
 router.use('/users', usersRouter);
 router.use('/example', exampleRouter);
 
+// router.use('/categories', categoriesRoutes);
+// router.use('/drinks', drinksRoutes);
+// router.use('/ingredients', ingredientsRoutes);
+// router.use('/users', usersRoutes);
+// router.use('/favorites', favoritesRoutes);
+
 module.exports = router;
