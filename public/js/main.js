a8f26006deda11b9d26bcad219754f90ae0d147c
Hanah R <hanahratican@gmail.com>
2023-04-04 08:58:20 -0700

    Edited all of the handlebars, still need to make refinements. Displayed all checkboxes in customDrink.hbs

--- public/js/main.js
+++
@@ -1,9 +0,0 @@
-// document.querySelector('.btn-logout').addEventListener('click', async () => {
-//   try {
-//     await fetch('/api/users/logout', { method: 'POST' });
-//     document.location.replace('/login');
-//   } catch (error) {
-//     console.error(error);
-//     console.error('Failed to logout.');
-//   }
-// });
